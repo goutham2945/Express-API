e358febd38e58d051578793c4b4cf67a22cbb40c
GouthamRaju <gouthamcdp@gmail.com>
2020-07-03 21:07:07 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,8 +2,8 @@ const config = require("./config")
 
 const getPaginatedResults = async function(req, results){
     self = results ? results: this
-    const pageNo = req.query.pageno ? parseInt(req.query.pageno) : 1
-    const perPage = req.query.perpage ? parseInt(req.query.perpage) : config.DEF_PER_PAGE_COUNT
+    const pageNo = req.query.pageno ? +req.query.pageno : 1
+    const perPage = req.query.perpage ? +req.query.perpage : config.DEF_PER_PAGE_COUNT
     return await self.skip((pageNo-1) * perPage).limit(perPage);
 }
 
